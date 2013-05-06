bbf1108a50313027c9081318f3cc5abbcffe4522
C. Vic Hu <vic@cvhu.org>
2013-05-06 00:18:56 -0500

    finished particle clustering draft coding

--- src/preprocessing/particleClustering.m
+++ src/preprocessing/particleClustering.m
@@ -16,10 +16,44 @@ for i = 1:N
    yi = mod(i,K)+1;
    X(i,:) = sampleTopic(yearTopics(yi,:)); 
 end
-display(yn);
-% display(X);
 
+for i = 1:(yn-1)
+    Z = linkage(X);
+    topics = yearTopics((i*K+1):((i+1)*K),:);
+    % dendrogram(Z);
+    C = cluster(Z, 'maxclust', 10);
+    C = classifyTopic(X, C, topics, K);
+    for j = 1:N
+        yi = i*K + C(j);
+        topic = yearTopics(yi,:);        
+        X(j,:) = shift2topic(X(j,:), topic);
+    end
+    display(dist(mean(X), topic));
+end
 
-function x = sampleTopic(topic)
+function Cout = classifyTopic(X,Cin,topics, K)
+Cout = Cin;
+for k = 1:K
+    ind = (Cin==k);
+    Xk = X(ind,:);
+    m = dist(mean(Xk),topics(1,:));
+    mk = 1;
+    for j = 2:K
+        c = dist(mean(Xk),topics(1,:));
+        if(c < m)
+            m = c;
+            mk = j;
+        end
+    end
+    Cout(ind) = mk;
+end
+
+function d = dist(x, y)
+d = (x*y')/(norm(x,2)*norm(y,2));
+
+function X = sampleTopic(topic)
 sigma = std(topic);
-x = topic + sigma*randn(size(topic));
+X = topic + sigma*randn(size(topic));
+
+function Xout = shift2topic(Xin, topic)
+Xout = 0.5*Xin + topic;
