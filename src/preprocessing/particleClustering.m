de88ee7d0ef9dd617507dd9cf6dba135e3a33c3a
C. Vic Hu <vic@cvhu.org>
2013-05-06 17:33:49 -0500

    setup experiments for various alpha and gamma

--- src/preprocessing/particleClustering.m
+++ src/preprocessing/particleClustering.m
@@ -5,14 +5,72 @@ all = dlmread('rr10.mat');
 K = 10;
 N = 100;
 Y = size(all,1)/K;
+gamma(K,N,Y,all,0.5);
+alpha(K,N,Y,all,0.5);
 
-dists = ['cosine', 'sqEuclidean', 'cityblock', 'correlation'];
-D = length(dists);
-dss = zeros(D,Y);
-for d = 1:D
-    dss(d,:) = run(all, K, N, dists(d), 'kmeans');
+% dists = {'cosine', 'sqEuclidean', 'cityblock', 'correlation'};
+% D = length(dists);
+% dss = zeros(D,Y);
+% for d = 1:D
+%     dss(d,:) = run(all, K, N, dists{1,d}, 'kmeans');
+% end
+% plot(dss')
+
+
+% dss = zeros(L,Y);
+% for l = 1:L
+%     dss(l,:) = run(all, K, N, dists{1,1}, links{1,l}, alpha, gamma);
+% end
+% semilogy(dss')
+
+function alpha(K,N,Y,all,gamma)
+ps = 0:0.25:1;
+L = length(ps);
+dss = zeros(L+1,Y);
+dss(1,:) = baseline(all, K);
+for l = 1:L
+    dss(l+1,:) = run(all, K, N, 'cosine', 'single', ps(l), gamma);
 end
-plot(dss')
+figure
+plot(dss(:,2:Y)');
+ylabel('Avg Error');
+xlabel('Year');
+title('Learning Curve of various \alpha');
+legend('baseline', '0.0', '0.25', '0.5', '0.75', '1.0');
+
+
+function gamma(K,N,Y,all,alpha)
+ps = 0:0.25:1;
+L = length(ps);
+dss = zeros(L+1,Y);
+dss(1,:) = baseline(all, K);
+for l = 1:L
+    dss(l+1,:) = run(all, K, N, 'cosine', 'single', alpha, ps(l));
+end
+figure
+plot(dss(:,2:Y)');
+ylabel('Avg Error');
+xlabel('Year');
+title('Learning Curve of various \gamma');
+legend('baseline', '0.0', '0.25', '0.5', '0.75', '1.0');
+
+
+
+function ds = baseline(yearTopics, K)
+Y = size(yearTopics)/K;
+ds = zeros(1,Y);
+for y = 2:Y
+    prev = yearTopics(((y-2)*K+1):(y-1)*K, :);
+    curr = yearTopics(((y-1)*K+1):y*K, :);
+    err = 0.0;
+    for k = 1:K
+        pi = prev(k,:);
+        mk = bestTopic(pi, curr);
+        err = err + distance(pi, curr(mk,:));
+    end
+    ds(y) = err/K;
+end
+
 
 % kmeans:
 % dist = cosine, sqEuclidean, cityblock, correlation
@@ -22,7 +80,7 @@ plot(dss')
 % dist = euclidean, cityblock, chebychev, cosine, correlation, jaccard
 % link = single, complete, average, weighted
 
-function [ds, Z] = run(yearTopics, K, N, dist, link)
+function [ds, Z] = run(yearTopics, K, N, dist, link, alpha, gamma)
 
 [h, w] = size(yearTopics);
 yn = h/K;  % number of years
@@ -35,8 +93,8 @@ X = zeros(N,w);
 err = 0.0;
 for i = 1:N   
    yi = mod(i,K)+1;
-   X(i,:) = sampleTopic(yearTopics(yi,:)); 
-   err = err + dist(X(i,:), yearTopics(yi,:));
+   X(i,:) = sampleTopic(yearTopics(yi,:), alpha); 
+   err = err + distance(X(i,:), yearTopics(yi,:));
 end
 ds = zeros(1,yn);
 ds(1) = err/N;
@@ -58,8 +116,8 @@ for i = 1:(yn-1)
     for j = 1:N
         yi = i*K + C(j);
         topic = yearTopics(yi,:);        
-        err = err + dist(X(j,:), topic);
-        X(j,:) = shift2topic(X(j,:), topic);        
+        err = err + distance(X(j,:), topic);
+        X(j,:) = shift2topic(X(j,:), topic, alpha, gamma);        
     end
     ds(i+1) = err/N;
 end
@@ -71,17 +129,19 @@ function Cout = classifyTopic(Xc,Cin,topics, K)
 Cout = Cin;
 for k = 1:K
     ind = (Cin==k);
-    Xk = Xc(k,:);
-    m = dist(Xk,topics(1,:));
-    mk = 1;
-    for j = 2:K
-        c = dist(mean(Xk),topics(1,:));
-        if(c < m)
-            m = c;
-            mk = j;
-        end
+    Xk = Xc(k,:);    
+    Cout(ind) = bestTopic(Xk, topics);
+end
+
+function mk = bestTopic(x, topics)
+m = distance(x,topics(1,:));
+mk = 1;
+for j = 2:size(topics,1)
+    c = distance(x,topics(j,:));
+    if(c < m)
+        m = c;
+        mk = j;
     end
-    Cout(ind) = mk;
 end
 
 function Xout = centroids(Xin, C, K)
@@ -92,13 +152,13 @@ for k = 1:K
 end
 
 
-function d = dist(x, y)
+function d = distance(x, y)
 d = 1 - (x*y')/(norm(x,2)*norm(y,2));
 
-function X = sampleTopic(topic)
-sigma = 0.5*std(topic);
+function X = sampleTopic(topic, alpha)
+sigma = alpha*std(topic);
 X = topic + sigma*randn(size(topic));
 
-function Xout = shift2topic(Xin, topic)
-sigma = 0.5*std(Xin);
-Xout = 0.1*Xin + topic + sigma*randn(size(Xin));
+function Xout = shift2topic(Xin, topic, alpha, gamma)
+sigma = alpha*std(topic);
+Xout = gamma*Xin + topic + sigma*randn(size(Xin));
